49812ac0f871f33563080fe474373a7e3991d02d
jkkim <m3616@naver.com>
2021-03-27 21:20:58 +0900

    Opencv Test

--- test.cpp
+++ test.cpp
@@ -6,19 +6,33 @@ using namespace cv;
 
 int main()
 {
-    cout<<"Hello OpenCV"<<CV_VERSION<<endl;    
+    Mat img1;       // Empty matrix
+    
+    Mat img2(480, 640, CV_8UC1);
+    Mat img3(480, 640, CV_8UC3);
+    Mat img4(Size(640, 480), CV_8UC3);
 
-    Mat img;
-    img = imread("lenna.bmp");
+    Mat img5(480, 640, CV_8UC1, Scalar(128));       // (128, 128, 128)
+    Mat img6(480, 640, CV_8UC3, Scalar(0,0,255));   // Red Color
 
-    if(img.empty())
-    {
-        cout<<"Image load failed!"<<endl;
-        return -1;
-    }
+    Mat mat1 = Mat::zeros(3,3, CV_32SC1);   // 0's Matrix
+    Mat mat2 = Mat::ones(3, 3, CV_32FC1);   // 1's Matrix
+    Mat mat3 = Mat::eye(3, 3, CV_32FC1);     // dentity matirx
 
-    namedWindow("image");
-    imshow("image", ~img);
+    float data[] = {1,2,3,4,5,6};
+    Mat mat4(2,3, CV_32FC1, data);
+
+    Mat mat5 = (Mat_<float>(2,3) << 1,2,3,4,5,6);
+    Mat mat6 = Mat_<uchar>({2,3}, {1,2,3,4,5,6});
+
+    mat4.create(256, 256, CV_8UC3);     // uchar, 3-channels
+    mat5.create(4,4,CV_32FC1);          // float, 1-channel
+
+    mat4 = Scalar(255,0,0);
+    mat5.setTo(1.0f);
+
+    imshow("mat4", mat4);
+    imshow("mat5", mat5);
 
     waitKey();
 }
